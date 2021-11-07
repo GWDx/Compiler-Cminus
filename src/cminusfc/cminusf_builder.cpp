aa8b708414e5324ceb7551f582a5e284e6349885
GWDx <gwdx@mail.ustc.edu.cn>
2021-11-07 15:23:16 +0800

    编写 cminusf_builder.cpp，仅仅可以生成 main 函数

--- src/cminusfc/cminusf_builder.cpp
+++ src/cminusfc/cminusf_builder.cpp
@@ -1,11 +1,9 @@
 #include "cminusf_builder.hpp"
+#include "logging.hpp"
 
 // use these macros to get constant value
-#define CONST_FP(num) \
-    ConstantFP::get((float)num, module.get())
-#define CONST_ZERO(type) \
-    ConstantZero::get(var_type, module.get())
-
+#define CONST_FP(num) ConstantFP::get((float)num, module.get())
+#define CONST_INT(num) ConstantInt::get(num, module.get())
 
 // You can define global variables here
 // to store state
@@ -18,34 +16,49 @@
  * scope.find: find and return the value bound to the name
  */
 
-void CminusfBuilder::visit(ASTProgram &node) { }
+void CminusfBuilder::visit(ASTProgram& node) {
+    LOG(INFO) << "Program";
+    for (auto i : node.declarations)
+        i->accept(*this);
+    std::cout << module->print();
+}
 
-void CminusfBuilder::visit(ASTNum &node) { }
+void CminusfBuilder::visit(ASTNum& node) {
+    LOG(INFO) << "Num";
+}
 
-void CminusfBuilder::visit(ASTVarDeclaration &node) { }
+void CminusfBuilder::visit(ASTVarDeclaration& node) {}
 
-void CminusfBuilder::visit(ASTFunDeclaration &node) { }
+void CminusfBuilder::visit(ASTFunDeclaration& node) {
+    LOG(INFO) << "FunDeclaration";
 
-void CminusfBuilder::visit(ASTParam &node) { }
+    Type* Int32Type = Type::get_int32_type(module.get());
+    auto mainFunction = Function::create(FunctionType::get(Int32Type, {}), node.id, module.get());
+    auto basicBlock = BasicBlock::create(module.get(), "entry", mainFunction);
+    builder->set_insert_point(basicBlock);
+    builder->create_ret(CONST_INT(0));
+}
 
-void CminusfBuilder::visit(ASTCompoundStmt &node) { }
+void CminusfBuilder::visit(ASTParam& node) {}
 
-void CminusfBuilder::visit(ASTExpressionStmt &node) { }
+void CminusfBuilder::visit(ASTCompoundStmt& node) {}
 
-void CminusfBuilder::visit(ASTSelectionStmt &node) { }
+void CminusfBuilder::visit(ASTExpressionStmt& node) {}
 
-void CminusfBuilder::visit(ASTIterationStmt &node) { }
+void CminusfBuilder::visit(ASTSelectionStmt& node) {}
 
-void CminusfBuilder::visit(ASTReturnStmt &node) { }
+void CminusfBuilder::visit(ASTIterationStmt& node) {}
 
-void CminusfBuilder::visit(ASTVar &node) { }
+void CminusfBuilder::visit(ASTReturnStmt& node) {}
 
-void CminusfBuilder::visit(ASTAssignExpression &node) { }
+void CminusfBuilder::visit(ASTVar& node) {}
 
-void CminusfBuilder::visit(ASTSimpleExpression &node) { }
+void CminusfBuilder::visit(ASTAssignExpression& node) {}
 
-void CminusfBuilder::visit(ASTAdditiveExpression &node) { }
+void CminusfBuilder::visit(ASTSimpleExpression& node) {}
 
-void CminusfBuilder::visit(ASTTerm &node) { }
+void CminusfBuilder::visit(ASTAdditiveExpression& node) {}
 
-void CminusfBuilder::visit(ASTCall &node) { }
+void CminusfBuilder::visit(ASTTerm& node) {}
+
+void CminusfBuilder::visit(ASTCall& node) {}
