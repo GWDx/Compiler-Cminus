53a0e75587e8031a63fde58377afdf9b5156b55a
GWDx <gwdx@mail.ustc.edu.cn>
2021-12-28 20:29:02 +0800

    add CMakeList

---
+++ include/CodeGenerate.hpp
@@ -0,0 +1,8 @@
+#ifndef ACTIVEVARS_HPP
+#define CODE_GENERATE_HPP
+
+class CodeGenerate {
+public:
+};
+
+#endif
--- init.sh
+++ init.sh
@@ -4,6 +4,3 @@ cd build
 cmake ..
 make -j 6
 clang -shared -o io.so ../src/io/io.c
-
-rm ../tests/3-ir-gen/libcminus_io.a
-cp libcminus_io.a ../tests/3-ir-gen
