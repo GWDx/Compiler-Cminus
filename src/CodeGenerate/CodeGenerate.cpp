d213f31c13468d38f365cb5457f23a51746831ab
GWDx <gwdx@mail.ustc.edu.cn>
2021-12-29 21:37:50 +0800

    返回常量整数

--- src/CodeGenerate/CodeGenerate.cpp
+++ src/CodeGenerate/CodeGenerate.cpp
@@ -2,7 +2,10 @@
 #include <string>
 #include <vector>
 
-std::string CodeGenerate::generate() {
+using std::string;
+using std::to_string;
+
+string CodeGenerate::generate() {
     int functionEndNumber = 0;
     append(".text");
     append(".globl main");
@@ -14,17 +17,29 @@ std::string CodeGenerate::generate() {
             append(function->get_name() + ":");
 
             appendTab(".cfi_startproc");
-            appendTab("retq");
+            appendTab("pushq	%rbp");
+            appendTab("movq	%rsp, %rbp");
 
             for (auto basicblock : function->get_basic_blocks()) {
+                for (auto instruction : basicblock->get_instructions())
+                    if (instruction->get_instr_type() == Instruction::ret) {
+                        // auto returnInstruction = dynamic_cast<ReturnInst*>(instruction);
+                        if (instruction->get_num_operand()) {
+                            auto value = instruction->get_operand(0);
+                            auto constantValue = dynamic_cast<ConstantInt*>(value);
+                            appendTab("mov	$" + to_string(constantValue->get_value()) + ", %eax");
+                        }
+                        appendTab("popq	%rbp");
+                        appendTab("retq");
+                    }
             }
-            append(".Lfunc_end" + std::to_string(functionEndNumber) + ":");
+            append(".Lfunc_end" + to_string(functionEndNumber) + ":");
             appendTab(".cfi_endproc");
             functionEndNumber++;
         }
     }
 
-    std::string ansCode;
+    string ansCode;
     for (auto line : lines)
         ansCode = ansCode + line + "\n";
     return ansCode;
