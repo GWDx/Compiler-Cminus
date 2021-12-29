666cc060fc35d42c32dfd68e00852d12b0387f96
GWDx <gwdx@mail.ustc.edu.cn>
2021-12-29 23:17:27 +0800

    加法

--- src/CodeGenerate/CodeGenerate.cpp
+++ src/CodeGenerate/CodeGenerate.cpp
@@ -1,12 +1,44 @@
 #include "CodeGenerate.hpp"
+#include <map>
 #include <string>
 #include <vector>
 
+using std::map;
 using std::string;
 using std::to_string;
 
+#define FOR(i, l, r) for (i = l; i <= r; i++)
+
+map<int, bool> allRegister;
+map<Value*, string> valueToRegister;
+
+string getEmptyRegister(Value* value) {
+    int i;
+    FOR (i, 8, 15)
+        if (allRegister[i]) {
+            allRegister[i] = false;
+            string ans = "%r" + to_string(i) + "d";
+            valueToRegister[value] = ans;
+            return ans;
+        }
+    return "";
+}
+
+string valueToRegOrConstant(Value* value) {
+    string ans;
+    auto constantValue = dynamic_cast<ConstantInt*>(value);
+    if (constantValue)
+        ans = "$" + to_string(constantValue->get_value());
+    else
+        ans = valueToRegister[value];
+    return ans;
+}
+
 string CodeGenerate::generate() {
-    int functionEndNumber = 0;
+    int i, functionEndNumber = 0;
+    FOR (i, 8, 15)
+        allRegister[i] = true;
+
     append(".text");
     append(".globl main");
     append(".p2align	4, 0x90");
@@ -19,19 +51,34 @@ string CodeGenerate::generate() {
             appendTab(".cfi_startproc");
             appendTab("pushq	%rbp");
             appendTab("movq	%rsp, %rbp");
+            // appendTab("subp	%rsp, 16");
 
             for (auto basicblock : function->get_basic_blocks()) {
-                for (auto instruction : basicblock->get_instructions())
-                    if (instruction->get_instr_type() == Instruction::ret) {
+                for (auto instruction : basicblock->get_instructions()) {
+                    Value* value = instruction;
+                    auto instructionType = instruction->get_instr_type();
+                    if (instructionType == Instruction::add) {
+                        auto value1 = instruction->get_operand(0);
+                        auto value2 = instruction->get_operand(1);
+                        auto regOrConstant1 = valueToRegOrConstant(value1);
+                        auto regOrConstant2 = valueToRegOrConstant(value2);
+
+                        auto reg = getEmptyRegister(value);
+                        appendTab("movl	" + regOrConstant1 + ", " + reg);
+                        appendTab("addl	" + regOrConstant2 + ", " + reg);
+                    }
+
+                    if (instructionType == Instruction::ret) {
                         // auto returnInstruction = dynamic_cast<ReturnInst*>(instruction);
                         if (instruction->get_num_operand()) {
                             auto value = instruction->get_operand(0);
-                            auto constantValue = dynamic_cast<ConstantInt*>(value);
-                            appendTab("mov	$" + to_string(constantValue->get_value()) + ", %eax");
+                            auto regOrConstant = valueToRegOrConstant(value);
+                            appendTab("movl	" + regOrConstant + ", %eax");
                         }
                         appendTab("popq	%rbp");
                         appendTab("retq");
                     }
+                }
             }
             append(".Lfunc_end" + to_string(functionEndNumber) + ":");
             appendTab(".cfi_endproc");
